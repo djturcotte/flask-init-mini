f5c1b0833201c764eda542881920e40405983767
Doug Turcotte <doug.turcotte1@gmail.com>
2022-08-05 14:30:58 -0400

    Update test_app.py

--- app/test_app.py
+++ app/test_app.py
@@ -1,11 +1,14 @@
 import pytest
+
 from run import app
+
 
 @pytest.fixture
 def client():
   app.config["TESTING"] = True
   with app.test_client() as client:
     yield client
+    
     
 def test_api(client):
   response = client.get("/")
